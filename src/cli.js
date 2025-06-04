05878d999582b9130042e3629c29ea2d519428ad
sobolevaea <liziaka@mail.ru>
2025-06-04 12:23:01 +0300

    Fix syntax cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync'
 
 const askName = () => {
-	console.log('Welcome to the Brain Games!')
+  console.log('Welcome to the Brain Games!')
   const userName = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${userName}!`)
 }
