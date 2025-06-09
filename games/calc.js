34844f3fdc7d4ccf01a7d5002951d08766eb239e
sobolevaea <liziaka@mail.ru>
2025-06-09 14:16:25 +0300

    Add calc.js

---
+++ games/calc.js
@@ -0,0 +1,29 @@
+import runGame from '../src/index.js'
+import generateNumber from '../src/utils.js'
+
+const description = ('What is the result of the expression?')
+const operators = ['+', '-', '*']
+
+const calc = (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2
+    case '-':
+      return operand1 - operand2
+    case '*':
+      return operand1 * operand2
+  }
+}
+
+const runCalc = () => {
+  const operator = operators[generateNumber(0, operators.length - 1)]
+  const value1 = generateNumber()
+  const value2 = generateNumber()
+
+  const question = `${value1} ${operator} ${value2}`
+  const answer = String(calc(value1, value2, operator))
+
+  return [question, answer]
+}
+
+runGame(description, runCalc)
