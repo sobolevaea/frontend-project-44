e524df9faa62543a7847e659d0ebbfc91c1afe5d
sobolevaea <liziaka@mail.ru>
2025-06-10 14:32:32 +0300

    Add progression.js

---
+++ games/progression.js
@@ -0,0 +1,31 @@
+import generateNumber from '../src/utils.js'
+import runGame from '../src/index.js'
+
+const description = 'What number is missing in the progression?'
+const maxLength = 10
+const maxStep = 20
+
+const createSequence = (start, step, length) => {
+  const sequence = []
+  for (let index = 0; index < length; index += 1) {
+    sequence[index] = start + index * step
+  }
+
+  return sequence
+}
+
+const generateRound = () => {
+  const start = generateNumber()
+  const step = generateNumber(1, maxStep)
+  const sequence = createSequence(start, step, maxLength)
+
+  const hiddenNumberIndex = generateNumber(0, sequence.length - 1)
+  const answer = String(sequence[hiddenNumberIndex])
+  const question = sequence.join(' ').replace(answer, '..')
+
+  return [question, answer]
+}
+
+const runProgression = () => runGame(description, generateRound)
+
+export default runProgression
