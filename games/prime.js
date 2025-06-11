f635344c71f1037b15500ad59b20e25455987d88
sobolevaea <liziaka@mail.ru>
2025-06-11 12:07:07 +0300

    Fix prime.js

--- games/prime.js
+++ games/prime.js
@@ -4,17 +4,19 @@ import runGame from '../src/index.js'
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (number) => {
-  if (number < 2 || number % 2 === 0) {
-    return false
-  }
-
   if (number === 2) {
     return true
+  }
+
+  if (number < 2 || number % 2 === 0) {
+    return false
   }
 
   for (let i = 3; i < Math.sqrt(number); i += 1) {
     return (number % i === 0 ? false : true)
   }
+
+  return true
 }
 
 const generateRound = () => {
