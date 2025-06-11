597e36c30e5b5c6c30377afc33d5ce57ace21deb
sobolevaea <liziaka@mail.ru>
2025-06-11 11:48:57 +0300

    Add prime.js

---
+++ games/prime.js
@@ -0,0 +1,30 @@
+import generateNumber from '../src/utils.js'
+import runGame from '../src/index.js'
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+const isPrime = (number) => {
+  if (number < 2 || number % 2 === 0) {
+    return false
+  }
+
+  if (number === 2) {
+    return true
+  }
+
+  for (let i = 3; i < Math.sqrt(number); i += 1) {
+    return (number % i === 0 ? false : true)
+  }
+}
+
+const generateRound = () => {
+  const number = generateNumber()
+  const question = String(number)
+  const answer = isPrime(number) ? 'yes' : 'no'
+
+  return [question, answer]
+}
+
+const runPrime = () => runGame(description, generateRound)
+
+export default runPrime
