b7cc79113836694aff1569fd906d14e39edf393d
sobolevaea <liziaka@mail.ru>
2025-06-13 16:14:24 +0300

    Fix exports

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,1 @@
-const generateNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min
-
-export default generateNumber
+export const generateNumber = (min = 0, max = 20) => Math.floor(Math.random() * (max - min + 1)) + min
